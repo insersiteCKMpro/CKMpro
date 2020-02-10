db6fa5abbcbe8150fddc36531250b53a2a7bc794
Your Name <you@example.com>
2020-02-10 11:10:25 +0100

    retraitmydoc

--- Php/02 -inclusions - fichiers/include/footer.php
+++
@@ -1,5 +0,0 @@
-   
-  <footer class="bg-secondary p-4 text-white text-center">
-             &copy; 2019 - Sebastien SAFOURCADE - touche pas a mon site !!!
-        </footer>
- 
--- Php/02 -inclusions - fichiers/include/nav.php
+++
@@ -1,8 +0,0 @@
-
-       
-        <nav class="bg-secondary text-center p-2">
-             <a href="acceuil.php" id="1" class="text-white alert-link pr-4">acceuil</a>
-             <a href="actualites.php"  id="2" class="text-white alert-link pr-4">actualites</a>
-             <a href="contact.php" id="3" class="text-white alert-link pr-4">contact</a>
-             <a href="quisommesnous.php" id="4" class="text-white alert-link pr-4">qui sommes nous</a>
-        </nav>
