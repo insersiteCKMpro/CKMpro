4484ff5161c832fd551ae447d039204fcef72b1c
imaneR78 <imaneremina@icloud.com>
2020-02-19 10:08:51 +0100

    contact ckmpro

--- header.php
+++ header.php
@@ -184,8 +184,7 @@
             .fixed-top
             {
               font-size:1.5em;
-            }
-            
+            }  
           }
 
             
@@ -413,7 +412,7 @@
 
       
     
-      < class="navbar navbar-expand-lg navbar-light rounded">
+      <div class="navbar navbar-expand-lg navbar-light rounded">
 
         <div class="mx-auto">
           <p class="textMenu"><strong>Menu</strong></p>
@@ -474,10 +473,7 @@
     </div>
   
   </header>
-<<<<<<< HEAD
-=======
-  
->>>>>>> 400e491d2da30e0616b70c140ea7012fc2e972cf
+
 
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
