8115527c5e557ba7b6a78ced534695f99c8a24ac
BastienG78 <bastien_78@hotmail.fr>
2020-02-25 23:05:57 +0100

    headercom

---
+++ headercommerce.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+
+    <title>CKMpro-Page-Secondaire</title>
+</head>
+
+<style>
+header
+{
+    /* background-color:red; */
+    height:40vh;
+}
+.lignehaut 
+{
+    display:flex;
+    justify-content:flex-end;
+    /* background-color:green; */
+    height:3vh;
+    margin-top:2vh;
+    font-size:1.5em;
+}
+.vitrine
+{
+    margin-left:1%;
+    margin-right:1%;
+
+}
+.tel
+{
+    margin-right:1%;
+    height:100%;
+    font-size:1.5em;
+}
+.lignebas
+{
+    display:flex;
+    align-items:flex-end;
+    justify-content:center;
+}
+.clicklogo
+{
+    margin-right:5%;
+}
+
+
+    /* RESPONSIVE */
+
+ /* PC */
+ @media screen and (max-width: 2560px) and (min-width: 769px) {
+    }
+
+
+    /*TABLETTE*/
+    @media screen and (max-width: 768px) and (min-width: 451px) {
+    }
+
+
+
+  /* telephone */
+    @media screen and (max-width: 450px) {
+    }
+</style>
+
+<header>
+<div class="container-fluid"></div>
+
+<div class="lignehaut">
+
+<img class="tel" src="img/telephonedesktop.png" alt="">
+<p class="num"> 01.25.74.89.12</p>
+<p class="vitrine"> CKM Pro Vitrine</p>
+</div>
+
+<div class="lignebas"> 
+
+<div class="clicklogo">
+<img src="img/click.png" alt="">
+</div>
+
+<div class="ckmlogo">
+<img src="img/CKMPROLOGODEFBOUTIQUE.png" alt="">
+</div>
+
+</div>
+
+
+
+</header>
+
+</div>
+</html>
