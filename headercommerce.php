34d0a1c590658ea1ddff591974dba5b1794d015c
BastienG78 <bastien_78@hotmail.fr>
2020-02-26 17:39:32 +0100

    newheader

--- headercommerce.php
+++ headercommerce.php
@@ -14,7 +14,7 @@
 header
 {
     /* background-color:red; */
-    height:40vh;
+    max-height:auto;
 }
 .lignehaut 
 {
@@ -31,39 +31,173 @@ header
     margin-right:1%;
 
 }
-.tel
-{
-    margin-right:1%;
-    height:100%;
-    font-size:1.5em;
-}
-.lignebas
+.info
 {
     display:flex;
-    align-items:flex-end;
-    justify-content:center;
 }
-.clicklogo
+.tel
 {
-    margin-right:5%;
+    margin-left:-2%;
+    height:100%;
+}
+.deuxlogo .logoboutique
+{
+    display:flex;
+    justify-content:center;
+    position:relative;
+}
+.deuxlogo .logoclick
+{
+    display:flex;
+    justify-content:center;
+    position:absolute;
+    left:10%;
+    top:15%;
 }
 
+.deuxlogo
+{
+    display:block;
+}
+.clickco
+{
+    width:100%;
+}
 
     /* RESPONSIVE */
 
- /* PC */
- @media screen and (max-width: 2560px) and (min-width: 769px) {
+    /* ECRAN 4K */
+    @media screen and (max-width: 2560px) and (min-width: 1439px) 
+    {
+
+.lignehaut
+{
+    font-size:1.3em;
+}
+
+.clickco
+{
+    width:100%;
+}
+.logo
+{
+    width:46%;
+}
+
     }
 
+ /* PC */
+ @media screen and (max-width: 1440px) and (min-width: 1023px) 
+ {
+    .clickco
+{
+    width:100%;
+}
+.clicko
+{
+    width:70%;
+    right:30%;
+    position:relative;
+    
+}
+
+    }
+
+    @media screen and (max-width: 1024px) and (min-width: 767px) 
+{
+    .logo
+{
+    display:flex;
+    justify-content:center;
+    position:relative;
+    width:47%;
+    margin-top:6%;
+}
+.deuxlogo .logoclick
+
+{
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    left: 10%;
+    top: 20%;
+    width: 11%;
+}
+.lignehaut
+{
+    font-size: 1.3em ;
+}
+
+
+}
 
     /*TABLETTE*/
-    @media screen and (max-width: 768px) and (min-width: 451px) {
+    @media screen and (max-width: 768px) and (min-width: 424px) {
+        .lignehaut
+        {
+            display:flex;
+            flex-direction:column;
+            height:10vh;
+            align-items:flex-end;
+        }
+
+        .info {
+    margin-right: 1%;
+    display: flex;
+    margin-left: 7%;
+}
+
+.tel {
+    margin-left: -9%;
+    height: 50%;
+}
+.num
+{
+    margin-left:10%;
+}
     }
 
 
 
   /* telephone */
-    @media screen and (max-width: 450px) {
+    @media screen and (max-width: 425px) {
+        .vitrine
+        {
+            margin-left:17%;
+        }
+        .logo
+        {
+            width:65%;
+            position:absolute;
+        }
+        .lignehaut
+        {
+            font-size:0.9em;
+            margin-bottom:5%;
+            width:100%;
+            display:flex;
+            flex-direction:row;
+            height:4vh;
+        }
+        .clickco
+        {
+            display:none;
+        }
+        .tel
+        {
+            margin-right:3%;
+            height:20%;
+        }
+        .num
+        {
+            margin-right:5%;
+        }
+        .info
+{
+    display:flex;
+    margin-right:25%;
+}
+
     }
 </style>
 
@@ -72,25 +206,24 @@ header
 
 <div class="lignehaut">
 
+<div class="info">
 <img class="tel" src="img/telephonedesktop.png" alt="">
 <p class="num"> 01.25.74.89.12</p>
+</div>
 <p class="vitrine"> CKM Pro Vitrine</p>
 </div>
 
-<div class="lignebas"> 
+<div class="deuxlogo">
 
-<div class="clicklogo">
-<img src="img/click.png" alt="">
+<div class="logoclick">
+<img class="clickco" src="img/click.png" alt="">
 </div>
 
-<div class="ckmlogo">
-<img src="img/CKMPROLOGODEFBOUTIQUE.png" alt="">
+<div class="logoboutique">
+<img class="logo"src="img/CKMPROLOGODEFBOUTIQUE.png" alt="">
 </div>
 
 </div>
-
-
-
 </header>
 
 </div>
