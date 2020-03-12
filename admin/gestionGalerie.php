10fd8c942e6cbe7ffa1fa4a4ffa9c61722946358
Seb-back75 <sebastien.l7516@gmail.com>
2020-03-12 12:20:58 +0100

    modif

--- admin/gestionGalerie.php
+++ admin/gestionGalerie.php
@@ -4,8 +4,8 @@ require_once('../include/init.php');
 extract($_POST);
 extract($_GET);
 
-echo '<pre>'; print_r($_POST);echo'<pre>';
-echo '<pre>'; print_r($_GET);echo'<pre>';
+//echo '<pre>'; print_r($_POST);echo'<pre>';
+//echo '<pre>'; print_r($_GET);echo'<pre>';
 
 
 
