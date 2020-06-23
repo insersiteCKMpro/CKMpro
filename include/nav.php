d3a8dfa0d2c46c14363ced5faa3b0697125e167b
BastienG78 <bastien_78@hotmail.fr>
2020-06-23 19:11:25 +0200

    ecrasement

--- include/nav.php
+++ include/nav.php
@@ -2,39 +2,14 @@
 
   <div class="container-fluid">
 
-  <nav class="navbar navbar-expand-sm navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light border border-secondary rounded ">
+  <a class="navbar-brand" href="index.php"><img src="img/CKMHOMEFOOTER.png" alt=""></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
+  <div class="collapse navbar-collapse mx-auto text-center" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-
-  <nav class="navbar col-12 navbar-expand-lg BackgroundNav border border-dark">
-  <!-- Brand/logo -->
-  <a class="navbar-brand" href="index.php">
-    <img src="img/CKMHOMEFOOTER.png" alt="logo">
-  </a>
-  
-  <!-- Links -->
-  <ul class="navbar-nav mx-auto text-center">
-        <li class="nav-item dropdown">
+    <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle h4 HoverJaune" href="3D.php" id="navbardrop" data-toggle="dropdown">
         3D
       </a>
@@ -58,7 +33,10 @@
       <a class="nav-link h4" href="contact.php">CONTACT</a>
     </li>
   </ul>
+  </div>
 </nav>
 
+
+  
 
   </div>
